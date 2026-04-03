68296b9acdf45c9b158231f9ca315c6b0c892969
Oleksandr Nykytin <alex_n@alma-soft.com>
2026-04-03 19:32:16 +0300

    Lambs

--- intro/03_collections/01_practic_cities.py
+++ intro/03_collections/01_practic_cities.py
@@ -1,6 +1,6 @@
 # Список городов для первого множества (с повторами)
 cities_list_1 = [
-    "Tokyo", "Delhi", "Shanghai", "Sao Paulo", "Mexico City",
+    "Tokyo", "Delhi", "Shanghai", "Sao Paulo", "Mexico City", "Kiev"
     "Cairo", "Mumbai", "Beijing", "Dhaka", "Osaka",
     "New York", "Karachi", "Buenos Aires", "Chongqing", "Istanbul",
     "Kolkata", "Manila", "Lagos", "Rio de Janeiro", "Tianjin",
@@ -21,3 +21,28 @@ cities_list_2 = [
 ]
 
 
+set_1 = set(cities_list_1)
+set_2 = set(cities_list_2)
+
+
+
+# 1 ----------------------------------------
+print("содержащее названия, которые есть в обоих множествах")
+cities1 = set_1 & set_2
+print(cities1, "\n")
+
+# 2 ----------------------------------------
+print("содержащее названия, которые есть в первом множестве, но нет во втором")
+cities2 = set_1 - set_2
+print(cities2, "\n")
+
+#3 ----------------------------------------
+print("содержащее названия, которые есть во втором множестве, но нет в первом")
+cities3 = set_2 - set_1
+print(cities3, "\n")
+
+#4 ----------------------------------------
+print("Необходимо создать третье множество, содержащее уникальные названия для каждого множества.")
+unique_cities = set_1 ^ set_2
+print(unique_cities, "\n")
+print(cities2 | cities3, "\n")
