b66f937265a31f609ea9c8d53e0b0759329f57f2
Oleksandr Nykytin <alex_n@alma-soft.com>
2026-05-11 18:57:29 +0300

    Add about page view

--- intro/django1/pages/urls.py
+++ intro/django1/pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from feedback.views import show_feedback
-from pages.views import home_page
+from pages.views import home_page, about_page
 
 app_name = 'pages'
 
 urlpatterns = [
     path('', home_page, name='home'),
+    path('about/', about_page, name='about'),
 ]
--- intro/django1/pages/views.py
+++ intro/django1/pages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'pages/home.html')
+
+def about_page(request):
+    return render(request, 'pages/about.html')
